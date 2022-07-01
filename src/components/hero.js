6530e0ffe83f900cbede2fb3b74a2b3bb7350a5d
Ibrahim Mato Dangada <31827659+imadangada@users.noreply.github.com>
2022-07-01 15:25:30 -0700

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -139,7 +139,7 @@ return (
                   bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-pink-500 to-purple-500 ">Blockchain CV</span>
                 </h1>
                 <p className="mt-5 text-base text-purple-200 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                  TCreate your own decentralized blockchain CV today.
+                  Create your own decentralized blockchain CV today.
                 </p>
                 <div className="mt-10 sm:mt-12">
                   <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
